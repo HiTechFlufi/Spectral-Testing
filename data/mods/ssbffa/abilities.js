9c5cd6c0a17c245db71887d018b91f81fa2ca905
Insist <DeathlyPlays@users.noreply.github.com>
2019-10-02 01:15:25 -0500

    SSBFFA: Add Prof Drix's Custom Ability

---
+++ data/mods/ssbffa/abilities.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**@type {{[k: string]: ModdedAbilityData}} */
+let BattleAbilities = {
+	"hazardsson": {
+		id: "hazardsson",
+		name: "Hazards Son",
+		desc: "(Tries to) set up a variety of hazards upon entry.",
+		onStart(pokemon) {
+			this.useMove("spikes", pokemon);
+			this.useMove("spikes", pokemon);
+			this.useMove("spikes", pokemon);
+			this.useMove("stealthrock", pokemon);
+			this.useMove("leechseed", pokemon);
+			this.useMove("toxicspikes", pokemon);
+			this.useMove("toxicspikes", pokemon);
+		},
+	},
+};
+
+exports.BattleAbilities = BattleAbilities;
