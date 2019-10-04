4c30c026845877ca2e6ba2e48eab6e96b631b05e
Bamd <bamdthebamd@gmail.com>
2019-10-04 11:11:36 +0600

    SSBFFA: Nerf Prof Drix (#16)

--- data/mods/ssbffa/abilities.js
+++ data/mods/ssbffa/abilities.js
@@ -17,6 +17,35 @@ let BattleAbilities = {
 			this.useMove("stickyweb", pokemon);
 		},
 	},
+	"fsubs": {
+		id: "fsubs",
+		name: "F Subs",
+		desc: "This Pokemon's Status moves have priority raised by 3 and is immune to priority. Ignores Substitute, screens, Mist and Safeguard. Uses Taunt, Toxic, Gastro Acid, Destiny Bond and Topsy Turvy on switch-in.",
+		shortDesc: "+3 priority Status and immune to priority. Ignores Sub, screens, Mist and Safeguard. Uses Taunt, Toxic, Gastro Acid, Destiny Bond and Topsy Turvy on switch-in.",
+		onModifyPriority(priority, pokemon, target, move) {
+			if (move && move.category === 'Status') {
+				move.fsubsBoosted = true;
+				return priority + 3;
+			}
+		},
+		onFoeTryMove(target, source, effect) {
+			if ((source.side === this.effectData.target.side || effect.id === 'perishsong') && effect.priority > 0.1 && effect.target !== 'foeSide') {
+				this.attrLastMove('[still]');
+				this.add('cant', this.effectData.target, 'ability: F Subs', effect, '[of] ' + target);
+				return false;
+			}
+		},
+		onModifyMove(move) {
+			move.infiltrates = true;
+		},
+		onStart(pokemon) {
+			this.useMove("gastroacid", pokemon);
+			this.useMove("toxic", pokemon);
+			this.useMove("taunt", pokemon);
+			this.useMove("destinybond", pokemon);
+			this.useMove("topsyturvy", pokemon);
+		},
+	},
 };
 
 exports.BattleAbilities = BattleAbilities;
