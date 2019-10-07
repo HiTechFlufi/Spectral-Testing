2ba968514c6d0a31ea86aab6b9f90d2f491c9846
Volco <13051425+Volco@users.noreply.github.com>
2019-10-06 23:59:57 -0400

    Update server.js

--- server/server-plugins/server.js
+++ server/server-plugins/server.js
@@ -352,7 +352,7 @@ exports.commands = {
 					return;
 				} else {
 					if (!definitions[0]) Rooms.get("development").add(`Urban Define crash info: target: ${target} | user: ${user.userid}`).update(); // i'm curious as to who keeps causing it
-					if (!definitions[0] || (definitions[0] && !definitions[0][`word`]) || (definitions[0] && !definitions[0][`definition`])) {
+					if (!definitions[0] || (definitions[0] && (!definitions[0][`word`] || !definitions[0][`definition`]))) {
 						this.sendReplyBox(`No results for <strong>"${target}"</strong>.`);
 						if (room) room.update();
 						return;
