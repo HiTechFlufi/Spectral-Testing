b07f6d8fed6e4bdd4616ebffcc1a0c480dc73aa0
Volco <13051425+Volco@users.noreply.github.com>
2019-10-19 17:01:55 -0400

    Update pets.js

--- server/server-plugins/pets.js
+++ server/server-plugins/pets.js
@@ -42,7 +42,7 @@ function load() {
 		let monId = toID(mon.species);
 		if (monId.indexOf("pokestar") !== -1) continue;
 		allPets.push(monId);
-		if (Dex.getTemplate(monId).battleOnly === true || Dex.getTemplate(monId).eventOnly === true || specialMons.indexOf(monId) !== -1 || Dex.getTemplate(monId).prevo.length || Dex.getTemplate(monId).tier === 'CAP') continue;
+		if (Dex.getTemplate(monId).battleOnly === true || Dex.getTemplate(monId).eventOnly === true || specialMons.indexOf(monId) !== -1 || Dex.getTemplate(monId).prevo.length || Dex.getTemplate(monId).tier === 'CAP' || Dex.getTemplate(monId).tier === 'CAP LC) continue;
 		randGenMons.push(monId);
 	}
 }
@@ -213,7 +213,7 @@ function getSprite(monName) {
 	}
 
 	let shiny = (toID(monName).includes("shiny") ? '-shiny' : '');
-	if (Dex.getTemplate(toID(monName)).tier === 'CAP') {
+	if (Dex.getTemplate(toID(monName)).tier === 'CAP' || Dex.getTemplate(toID(monName)).tier === 'CAP LC') {
 		output += `<img src="//play.pokemonshowdown.com/sprites/gen5${shiny}/${spriteid}.png">`;
 	} else {
 		output += `<img src="//play.pokemonshowdown.com/sprites/xyani${shiny}/${spriteid}.gif">`;
